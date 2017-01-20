2050ed33398c89c100f6e6544f57e7f52eee5d0a
doJester13 <minarini.work@gmail.com>
2017-01-20 23:57:03 +0100

    base

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+	public GameObject player;
+	public PlayerController pc;
+
+	void OnCollisionEnter(Collision collision){
+		Rigidbody body = player.GetComponent<Rigidbody> ();
+
+		Vector3 normal = collision.contacts[0].normal;
+		Vector3 vel = body.velocity;
+		float angle = Vector3.Angle (vel, -normal);
+		// measure angle
+		if (angle < 90) {
+			//sinistra
+			//player.GetComponent<PlayerController>().isOrbiting = true;
+			pc.isOrbiting = true;
+			Debug.Log("sinistra");
+		} else if (angle > 90) {
+			//destra
+			Debug.Log("destra");
+			body.transform.Rotate(0,-20*Time.deltaTime,0);
+		} else {
+			//crash
+			Debug.Log("boom");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public bool isOrbiting;
+	void Start () {
+		Rigidbody body = GetComponent<Rigidbody> ();
+		Vector3 push = new Vector3 (0.0f, 5.0f, 0.0f);
+		body.AddForce (push * speed);
+	}
+
+	void Update(){
+		if (isOrbiting) {
+			Vector3 verse = new Vector3 (0.0f, 0.0f, 1.0f);
+			transform.RotateAround(Vector3.zero, verse , 100 * Time.deltaTime);
+		}
+
+	}
+}
