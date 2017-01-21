9a322297580cc5718b512280c7ef620a81f5d15d
doJester13 <minarini.work@gmail.com>
2017-01-21 07:08:28 +0100

    buono da sx

--- Assets/Scripts/Orbit.cs
+++ Assets/Scripts/Orbit.cs
@@ -7,34 +7,36 @@ public class Orbit : MonoBehaviour {
 	public GameObject player;
 	public PlayerController pc;
 
-	void OnCollisionEnter(Collision collision){
-		Rigidbody body = player.GetComponent<Rigidbody> ();
+//	void OnCollisionEnter(Collision collision){
+//		Rigidbody body = player.GetComponent<Rigidbody> ();
+//
+//		Vector3 normal = collision.contacts[0].normal;
+//		Vector3 vel = body.velocity;
+//		//Debug.Log ("Collision: " + vel);
+//		float angle = Vector3.Angle (vel, -normal);
+//		// measure angle
+//
+//		Vector3 dir = Vector3.Reflect(body.transform.position, Vector3.right);
+//		Debug.Log ("DIR: " + dir);
+//		Debug.Log ("ANGLE: " + angle);
+//
+//		if (dir.x < -0.5) {
+//			Debug.Log(angle);
+//			pc.transform.Rotate (new Vector3(0f, 0f, 1f) * (180 - angle));
+//			body.velocity = Vector3.zero;
+//			body.isKinematic = true;
+//			pc.orbitingVerse = -1;
+//		} else if (dir.x > 0.5) {
+//			pc.transform.Rotate (new Vector3(0f, 0f, -1f) * (180 - angle));
+//			body.velocity = Vector3.zero;
+//			body.isKinematic = true;
+//			pc.orbitingVerse = 1;
+//		} else {
+//			GetComponent<SphereCollider> ().enabled = false;
+//			body.isKinematic = false;
+//			pc.orbitingVerse = 0;
+//		}
 
-		Vector3 normal = collision.contacts[0].normal;
-		Vector3 vel = body.velocity;
-		//Debug.Log ("Collision: " + vel);
-		float angle = Vector3.Angle (vel, -normal);
-		// measure angle
-
-		Vector3 dir = Vector3.Reflect(body.transform.position, Vector3.right);
-		//Debug.Log (dir);
-
-		if (dir.x < -0.5) {
-			Debug.Log(angle);
-			pc.transform.Rotate (new Vector3(0f, 0f, 1f) * (180 - angle));
-			body.velocity = Vector3.zero;
-			body.isKinematic = true;
-			pc.orbitingVerse = -1;
-		} else if (dir.x > 0.5) {
-			pc.transform.Rotate (new Vector3(0f, 0f, -1f) * (180 - angle));
-			body.velocity = Vector3.zero;
-			body.isKinematic = true;
-			pc.orbitingVerse = 1;
-		} else {
-			GetComponent<SphereCollider> ().enabled = false;
-			body.isKinematic = false;
-			pc.orbitingVerse = 0;
-		}
 //		Debug.Log("angolo: " + angle);
 //		if (angle < 180) {
 //			//destra
@@ -46,5 +48,5 @@ public class Orbit : MonoBehaviour {
 //			//crash
 //			Debug.Log("boom");
 //		}
-	}
+//	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public GameObject controller;
+
 	public int orbitingVerse;
 
 	private Vector3 prevVel;
@@ -21,9 +23,15 @@ public class PlayerController : MonoBehaviour {
 		Vector3 verse = new Vector3 (0.0f, 0.0f, 1.0f * orbitingVerse);
 		if (orbitingVerse == -1) {
 			// Orario
+
+			body.velocity = Vector3.zero;
+			body.isKinematic = true;
 			transform.RotateAround (Vector3.zero, verse, 100 * Time.deltaTime);
 		} else if (orbitingVerse == 1) {
 			// Antiorario
+
+			body.velocity = Vector3.zero;
+			body.isKinematic = true;
 			transform.RotateAround (Vector3.zero, verse, 100 * Time.deltaTime);
 		} else if (orbitingVerse == 0) {
 			body.velocity = prevVel;
@@ -43,5 +51,42 @@ public class PlayerController : MonoBehaviour {
 		}
 
 	}
+
+	void OnCollisionEnter(Collision c){
+		Vector3 fwd = GetComponent<Rigidbody> ().transform.right;
+		Vector3 point = c.contacts [0].point;
+		Vector3 pos = GetComponent<Rigidbody> ().position;
+
+		Vector3 normal = c.contacts[0].normal;
+		Vector3 vel = GetComponent<Rigidbody> ().velocity;
+		float angleOne = Vector3.Angle (vel, -normal);
+
+
+		Vector3 result = point - pos;
+		float angle = Vector3.Angle (result, fwd);
+					
+		float signedAngle = SignedAngleBetween (result, fwd, transform.position.normalized);
+		Debug.Log (angle);
+		if (signedAngle > 0) {
+			transform.Rotate (new Vector3(0f, 0f, 1f) * (180+angleOne));
+			orbitingVerse = 1;
+		} else if (signedAngle < 0) {
+			orbitingVerse = -1;
+			transform.Rotate (new Vector3(0f, 0f, -1f) * (180+angleOne));
+		} else {
+			orbitingVerse = 0;
+		}
+
+	}
+
+	float SignedAngleBetween(Vector3 a, Vector3 b, Vector3 n){
+		// angle in [0,180]
+		float angle = Vector3.Angle(a,b);
+		float sign = Mathf.Sign(Vector3.Dot(n,Vector3.Cross(a,b)));
+		// angle in [-179,180]
+		float signed_angle = angle * sign;
+		return signed_angle;
+	}
+
 
 }
